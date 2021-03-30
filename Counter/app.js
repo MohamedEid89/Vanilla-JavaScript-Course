de072e8150c35679a8273c9aace28c02603ee00a
Mohammed Eid <info@welayer.se>
2021-03-30 23:31:23 +0200

    Count 2d

---
+++ Counter/app.js
@@ -0,0 +1,31 @@
+/*------value = 0 --------*/
+let count = 0;
+
+/*------click btn--------*/
+const value = document.querySelector("#value");
+const btnclick = document.querySelectorAll(".btn");
+
+/*------function --------*/
+btnclick.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("dec")) {
+      count--;
+    } else if (styles.contains("inc")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
